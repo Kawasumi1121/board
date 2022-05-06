4a51279cc6f2c8b8747d18b126a16ba22c647717
kawasumi <wqgxy387@yahoo.co.jp>
2022-05-06 12:56:22 +0900

    一覧の表示 #3

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -6,12 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.board.repository.PostFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.board.repository.PostRepository;
+import com.example.board.repository.Post;
 
 /**
  * 掲示板のフロントコントローラー.
  */
 @Controller
 public class BoardController {
+	
+	/** 投稿の一覧 */
+	@Autowired
+	private PostRepository repository;
+	
 
 	/**
 	 * 一覧を表示する。
@@ -22,6 +30,21 @@ public class BoardController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
 		model.addAttribute("form", PostFactory.newPost());
+		model = this.setList(model);
+		model.addAttribute("path", "create");
+		
 		return "layout";
+	}
+	
+	/**
+	 * 一覧を設定する。
+	 *
+	 * @param model モデル
+	 * @return 一覧を設定したモデル
+	 */
+	private Model setList(Model model) {
+		Iterable<Post> list = repository.findAll();
+		model.addAttribute("list", list);
+		return model;
 	}
 }
